4845fa3b53309ed3b556380e6464897f4a703aab
Ethan El Kerchi--Plouton <eel-kerc@z4r4p5.42lyon.fr>
2026-01-08 10:13:29 +0100

    start of rotate

--- op_rotate_rev.c
+++ op_rotate_rev.c
@@ -6,8 +6,26 @@
 /*   By: eel-kerc <eel-kerc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 15:42:22 by eel-kerc          #+#    #+#             */
-/*   Updated: 2026/01/07 15:47:05 by eel-kerc         ###   ########.fr       */
+/*   Updated: 2026/01/07 16:13:31 by eel-kerc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 // Same as rotate but with loop in other sense
+
+void	ra(t_stack *stack_a)
+{
+	int	tmp;
+	
+	if (!stack_a)
+		return ;
+	while (stack_a->rank != 0)
+		stack_a = stack_a->next;
+	while ((stack_a->next)->rank != 0)
+	{	
+		tmp = stack_a->value;
+		stack_a->value = (stack_a->next)->value;
+		(stack_a->next)->value = tmp;
+		
+	}
+}
